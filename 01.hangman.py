7f1060c7a6828b597ada5e941acf874791566789
qbxof93 <79719733+jrobaczewski@users.noreply.github.com>
2022-06-28 12:41:05 +0100

    Update 01.hangman.py
    pep8 change

--- 01.hangman.py
+++ 01.hangman.py
@@ -1,43 +1,43 @@
 import sys
 
-numOfTries = 5
+num_of_tries = 5
 word = 'Enter'.upper()
-usedLetters = []
-userWord = []
+used_letters = []
+user_word = []
 
-def findIndexes(word, letter):
+def find_indexes(word, letter):
     indexes = []
-    for index, letterInWord in enumerate(word):
-        if letter == letterInWord:
+    for index, letter_in_word in enumerate(word):
+        if letter == letter_in_word:
             indexes.append(index)
     return indexes
 
-def showStateOfGame():
+def show_state_of_game():
     print()
-    print('Good Letters: ', userWord)
-    print('Attempts remain: ', numOfTries)
-    print('Letters used: ', usedLetters)
+    print('Good Letters: ', user_word)
+    print('Attempts remain: ', num_of_tries)
+    print('Letters used: ', used_letters)
     print()
 # 
 for _ in word:
-    userWord.append('_')
+    user_word.append('_')
 
 while True:
     letter = input('Enter the letter: ').upper()
-    usedLetters.append(letter)
-    foundIndexes = findIndexes(word, letter)
+    used_letters.append(letter)
+    foundIndexes = find_indexes(word, letter)
     if len(foundIndexes) == 0:
         print('No letter in the word!')
-        numOfTries -= 1
+        num_of_tries -= 1
     # Game Over
-        if numOfTries == 0:
+        if num_of_tries == 0:
             print('Game Over! :(')
             sys.exit(0)
     else: 
         for index in foundIndexes:
-            userWord[index] = letter
-        if ''.join(userWord) == word:
+            user_word[index] = letter
+        if ''.join(user_word) == word:
             print('Well done! You win the game!')
             sys.exit(0)
     
-    showStateOfGame()
+    show_state_of_game()
